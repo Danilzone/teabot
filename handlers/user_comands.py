f4be442ee4b1192004739baf6d0331fb4c096bd1
Danilzone <Anton990.bel@gmail.com>
2024-03-06 21:29:04 +0300

    sleep update

--- handlers/user_comands.py
+++ handlers/user_comands.py
@@ -37,78 +37,93 @@ async def start(message : Message):
 
 @router.message(Command(commands=['tea', 'чай']))
 async def tea(message : Message):
-    date = datetime.datetime.now()
-    check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'tea')
-    
-    if check[0]:
-        lit = round(random.uniform(0.3, 6),2)
-        res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'tea')
-        await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> чая\nВыпито всего <b>{res}</b> л." )
-    else: 
-        await message.answer(f"Чайник ещё не вскипел🫖\nПодожди <b>{check[1]}</b> мин.")
-
+    try:
+        date = datetime.datetime.now()
+        check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'tea')
+        
+        if check[0]:
+            lit = round(random.uniform(0.3, 6),2)
+            res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'tea')
+            await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> чая\nВыпито всего <b>{res}</b> л." )
+        else: 
+            await message.answer(f"Чайник ещё не вскипел🫖\nПодожди <b>{check[1]}</b> мин.")
+    except Exception:
+        console.print_exception(show_locals=True)
 
 @router.message(Command(commands=['coffee', 'coffe', 'cofe', 'кофе']))
 async def coffee(message : Message):
-    date = datetime.datetime.now()
-    check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'coffee')
+    try:
+        date = datetime.datetime.now()
+        check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'coffee')
 
-    if check[0]:
-        lit = round(random.uniform(0.3, 6),2)
-        res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'coffee')
-        await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> кофе\nВыпито всего <b>{res}</b> л." )
-    else: 
-        await message.answer(f"Кофе ещё не сварился😒\nПодожди <b>{check[1]}</b> мин.")
-
+        if check[0]:
+            lit = round(random.uniform(0.3, 6),2)
+            res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'coffee')
+            await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> кофе\nВыпито всего <b>{res}</b> л." )
+        else: 
+            await message.answer(f"Кофе ещё не сварился😒\nПодожди <b>{check[1]}</b> мин.")
+    except Exception:
+        console.print_exception(show_locals=True)
 
 @router.message(Command(commands=['juice', 'juc', 'сок']))
 async def juice(message : Message):
-    date = datetime.datetime.now()
-    check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'juice')
-    
-    if check[0]:
-        lit = round(random.uniform(0.3, 6),2)
-        res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'juice')
-        await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> сока\nВыпито всего <b>{res}</b> л." )
-    else: 
-        await message.answer(f"Сок ещё не привезли🧃\nПодожди <b>{check[1]}</b> мин.")
+    try:
+        date = datetime.datetime.now()
+        check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'juice')
+        
+        if check[0]:
+            lit = round(random.uniform(0.3, 6),2)
+            res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'juice')
+            await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> сока\nВыпито всего <b>{res}</b> л." )
+        else: 
+            await message.answer(f"Сок ещё не привезли🧃\nПодожди <b>{check[1]}</b> мин.")
+    except Exception:
+        console.print_exception(show_locals=True)
 
 @router.message(Command(commands=['beer', 'пиво', 'пивас', 'пивасик']))
 async def beer(message : Message):
-    date = datetime.datetime.now()
-    check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'beer')
-    
-    if check[0]:
-        lit = round(random.uniform(0.3, 6),2)
-        res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'beer')
-        await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> пива\nВыпито всего <b>{res}</b> л." )
-    else: 
-        await message.answer(f"📛Пиво не по акции!📛\nПодожди <b>{check[1]}</b> мин.")
+    try:
+        date = datetime.datetime.now()
+        check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'beer')
+        
+        if check[0]:
+            lit = round(random.uniform(0.3, 6),2)
+            res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'beer')
+            await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> пива\nВыпито всего <b>{res}</b> л." )
+        else: 
+            await message.answer(f"📛Пиво не по акции!📛\nПодожди <b>{check[1]}</b> мин.")
+    except Exception:
+        console.print_exception(show_locals=True)
 
 @router.message(Command(commands=['wine', 'вино']))
 async def wine(message : Message):
-    date = datetime.datetime.now()
-    check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'wine')
-    
-    if check[0]:
-        lit = round(random.uniform(0.3, 6),2)
-        res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'wine')
-        await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> вина\nВыпито всего <b>{res}</b> л." )
-    else: 
-        await message.answer(f"Время для вина еще не подошло🥴🍷\nПодожди <b>{check[1]}</b> мин.")
+    try: 
+        date = datetime.datetime.now()
+        check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'wine')
+        
+        if check[0]:
+            lit = round(random.uniform(0.3, 6),2)
+            res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'wine')
+            await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> вина\nВыпито всего <b>{res}</b> л." )
+        else: 
+            await message.answer(f"Время для вина еще не подошло🥴🍷\nПодожди <b>{check[1]}</b> мин.")
+    except Exception:
+        console.print_exception(show_locals=True)
 
 @router.message(Command(commands=['champagne', 'cham', 'champ', 'шампанское', 'шампунь', 'шам']))
 async def champagne(message : Message):
-    date = datetime.datetime.now()
-    check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'champagne')
-        
-    if check[0]:
-        lit = round(random.uniform(0.3, 6),2)
-        res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'champagne')
-        await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> шампанского\nВыпито всего <b>{res}</b> л." )
-    else: 
-        await message.answer(f"Время для шампуня еще не подошло🥴🍾\nПодожди <b>{check[1]}</b> мин.")
-
+    try:
+        date = datetime.datetime.now()
+        check = db.time_check(message.from_user.id, message.from_user.username, message.from_user.full_name, date, 'champagne')
+            
+        if check[0]:
+            lit = round(random.uniform(0.3, 6),2)
+            res = db.drank(message.from_user.id, message.from_user.username, message.from_user.full_name, lit, date, 'champagne')
+            await message.answer(f"{message.from_user.first_name} выпил <b>{lit} л.</b> шампанского\nВыпито всего <b>{res}</b> л." )
+        else: 
+            await message.answer(f"Время для шампуня еще не подошло🥴🍾\nПодожди <b>{check[1]}</b> мин.")
+    except Exception:
+        console.print_exception(show_locals=True)
     
 @router.message(Command(commands=["top"]))
 async def top(message : Message, command: CommandObject):
--- sleep.py
+++ sleep.py
@@ -1,19 +1,25 @@
-from config import token
-from rich import print
+from config import token, bot_commands
+
 import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 
-from aiogram import Bot, Dispatcher, executor, types
-
-
-bot = Bot(token)
+bot = Bot(token, parse_mode='html')
 dp = Dispatcher()
 
 
+@dp.message()
+async def echo(message: Message):
+    await message.answer("⚙️Бот чиниться🛠")
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer('Бот скоро заработает. Мы "делаем все" чтоб оно работал')
+async def main():
+
+    await bot.set_my_commands(bot_commands)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, on_startup=bot_commands)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == "__main__":
+    print("BOT SLEEP")
+    asyncio.run(main())
